7b8db25e9efefe5a86ecaf11bc2eed0a2f4f4c7a
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2025-09-29 14:40:28 +0200

    code update

--- gemini-api.py
+++ gemini-api.py
@@ -3,4 +3,16 @@ from google.genai import types
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
+# Only run this block for Gemini Developer API
+client = genai.Client()
 
+MODEL_ID = 'gemini-2.0-flash'
+
+response_1 = client.models.generate_content(
+    model=MODEL_ID,
+    contents='Hello, how are you',
+)
+
+print(response_1)
+
+response_1.
